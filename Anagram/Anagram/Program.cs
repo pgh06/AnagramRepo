ac530c87a7420778c8a4c9c97d65447b7f17012c
Peter Hansen <phansen@brainshark.com>
2020-06-11 13:19:00 -0400

    initial commit

---
+++ Anagram/Anagram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+
+        public static bool IsAnagram(string text1, string text2)
+        {
+            var version1 = text1.ToCharArray();
+            var version2 = text2.ToCharArray();
+            Array.Sort(version1);
+            Array.Sort(version2);
+            return version1.ToString() == version2.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string text1 = "racecar";
+            string text2 = "racecar";
+
+            var isAnagram = IsAnagram(text1, text2);
+        }
+    }
+}
